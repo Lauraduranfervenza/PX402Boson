582fe91a750edfcb50edbbe4de04f7b18a56ee06
Laura Duran_Fervenza <Lauraduranfervenza@gmail.com>
2021-10-19 16:10:56 +0100

    init

--- Zmass.py
+++ Zmass.py
@@ -1,11 +1,11 @@
- data_path = "/storage/epp2/phshgg/Public/MPhysProject_2021_2022/13TeV__2018__magnet_down_data__Z_candidates.root"
+data_path = "/storage/epp2/phshgg/Public/MPhysProject_2021_2022/13TeV__2018__magnet_down_data__Z_candidates.root"
 print(data_path)
 
 import ROOT as r
 import math as m
 input_file = r.TFile(data_path)
 input_file.ls()
-histp = r.TH1F('hist','',100,0.,100.)
+hist = r.TH1F('hist','',100,0,100.)
 
 tree = input_file.Get('DecayTree')
 
@@ -20,15 +20,30 @@ for entry in tree:
 
     mup_p = mup_PT*r.cosh(mup_ETA)
     mup_theta = 2*m.atan(m.e**(-mup_ETA))
-    mup_pz = p*r.cos(mup_theta)
+    mup_pz = mup_p*r.cos(mup_theta)
     mup_px =mup_PT*r.cos(mup_PHI)
     mup_py =mup_PT*r.sin(mup_PHI)
 
-    mup_E = r.sqrt(mup_p*mup_p + mass_muon*mass_muon)
-    mup_mass= mup_E*mup_E-mup_px*mup_px-mup_py*mup_py-mup_pz*mup_pz
-    histp.Fill(mup_mass)
-c = r.TCanvas('wmass')
-c.cd()
-histp.Draw()
-c.Print('wmass.png')
+    mum_PT = entry.mum_PT
+    mum_ETA = entry.mum_ETA
+    mum_PHI = entry.mum_PHI
 
+    mum_p = mum_PT*r.cosh(mum_ETA)
+    mum_theta = 2*m.atan(m.e**(-mum_ETA))
+    mum_pz = mum_p*r.cos(mum_theta)
+    mum_px =mum_PT*r.cos(mum_PHI)
+    mum_py =mum_PT*r.sin(mum_PHI)
+
+    mup_E = r.sqrt(mup_p*mup_p + mass_muon*mass_muon)/100
+    mum_E = r.sqrt(mum_p*mum_p + mass_muon*mass_muon)/100
+
+    E = mup_E
+    px = mup_px + mum_px
+    py = mup_py + mum_py
+    pz = mup_pz + mum_pz
+
+    mass= r.sqrt(E*E-px*px-py*py-pz*pz)
+    hist.Fill(mass)
+c = r.TCanvas('')
+hist.Draw()
+c.Print('zmass.png')
